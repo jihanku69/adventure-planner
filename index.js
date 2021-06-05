fadcbbf06b594b428747fd5f0c91f2ee30a2c7ec
Lindsey Howard <25946305+CodeLikeAGirl29@users.noreply.github.com>
2021-06-05 12:58:55 -0400

    fixed button error and a new look on the app

--- index.js
+++ index.js
@@ -1,66 +1,95 @@
-
-function getLongLat(location){
-	console.log($('.js-query').val());
-  $.getJSON('https://maps.googleapis.com/maps/api/geocode/json',{
-    address:`${location}`,
-    key:'AIzaSyB4Xc0nTrdkoE2k7F0vnJ-mLfKyTF5wqSE'
-		}, 
-		function(response){
-			console.log(response);
-			let lat = response.results[0].geometry.location.lat;
-			let lng = response.results[0].geometry.location.lng;
-			getWeatherData(lat, lng);
-			weatherHeader(response);
-
-	});
-}
-
-function getWeatherData() {
-    let city = $('.search-query').val();
-    $.ajax(WEATHER_SEARCH_URL, {
-        data: {
-            units: 'imperial',
-            q: city
-        },
-        dataType: 'jsonp',
-        type: 'GET',
-        success: function (data) {
-            let widget = displayWeather(data);
-            $('#weather-display').html(widget);
-            scrollPageTo('#weather-display', 15);
-        }
-    });
-}
-
-function displayWeather(data) {
-    return `
-    <div class="weather-results">
-        <h1><strong>Current Weather for ${data.name}</strong></h1>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <p style="font-size:30px; margin-top:10px;">${data.weather[0].main}</p>
-        <p style="color:steelblue;" ">Description:</p><p"> ${data.weather[0].description}</p>
-        <p style="color:steelblue;">Temperature:</p><p> ${data.main.temp} &#8457; / ${(((data.main.temp)-32)*(5/9)).toFixed(2)} &#8451;</p>
-        <p style="color:steelblue;">Min. Temperature:</p><p> ${data.main.temp_min} &#8457; / ${(((data.main.temp_min)-32)*(5/9)).toFixed(2)} &#8451</p>
-        <p style="color:steelblue;">Max. Temperature:</p><p> ${data.main.temp_max} &#8457; / ${(((data.main.temp_max)-32)*(5/9)).toFixed(2)} &#8451</p>
-        <p style="color:steelblue;">Humidity:</p><p> ${data.main.humidity} &#37;</p>
-    </div>
-`;
-}
-
-function enterLocation(){
-  $('.location-form').submit(event => {
-  event.preventDefault();
-  let query = $('.js-query').val();
-  $('.js-query').val("");
-  getLongLat(query);
-  $('.navigation').removeClass('hide');
-  })
-}
-
-function activatePlacesSearch(){
-  let input = document.getElementById('autocomplete')
-  let autocomplete = new google.maps.places.Autocomplete(input);
-}
-
-$(enterLocation);
-
+ /*** API URLS ***/
+ const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=ad19330a1a85eb38a78e85ceaba0c320';
+ const HIKING_PROJECT_URL = 'https://www.hikingproject.com/data/get-trails?maxDistance=10&key=200239759-6ef441eb317cb8f0a1c5f5220bf0cbc2';
+ 
+ /*** Get weather data from openWeatherMap API ***/
+ function getWeatherData() {
+     let city = $('#city-name').val();
+     $('#city-name').val('');
+     
+     const settings = {
+         url: WEATHER_URL,
+         data: {
+               q: city,
+               units: 'Imperial'
+         },
+         dataType: 'jsonp',
+         type: 'GET',
+         success: displayWeather
+       };
+       $.ajax(settings)
+       .fail(function(){
+           $('.weather-result').html("");
+           $('.hike-result').html(`<h2 class="error-msg"> Please enter a valid city name</h2>`);
+       });
+ };
+ 
+ /*** Display weather data ***/
+ function displayWeather(data) {
+     $('.weather-result').html(getWeatherDataTemplate(data));
+     getHikeTrailsData(data.coord.lat, data.coord.lon, displayHikeTrails);
+ };
+ 
+ /*** Get hiking trails data from REI Hiking project API ***/
+ function getHikeTrailsData(latitude, longitude, callback) {
+     const settings = {
+         url: HIKING_PROJECT_URL,
+         data: {
+             sort: 'distance',
+             lat: latitude,
+             lon: longitude
+         },
+         dataType: 'json',
+         type: 'GET',
+         success: callback
+     };
+     $.ajax(settings);
+ };
+ 
+ /*** Display hiking trails ***/
+ function displayHikeTrails(data) {
+     $('.hike-result').html("");
+     let trailList = data.trails;
+ 
+     if(trailList.length === 0) {
+         $('.hike-result').append(`<h2 class="error-msg"> No nearby trails found</h2>`);
+     }
+     else
+     {
+         for (let i = 0; i < trailList.length; i ++) {
+             let placename = encodeURI(trailList[i].name);
+             let result = hikeTrailTemplate(trailList[i], placename);
+             $('.hike-result').append(result);
+         }	
+     }  
+ };
+ 
+ /*** Autocomplete the input text using Google map API ***/
+ function  autocompleteSearchText()  {
+     let input = $('#city-name')[0];
+     let options = {
+         types: ['(regions)']
+     };
+     autocomplete = new google.maps.places.Autocomplete(input, options);
+ }
+ 
+ //Scrolls the page to the result section of the page
+ function scrollpage() {
+     $('.submit-button').click(function() {
+         $('html, body').animate({
+         scrollTop: $("#weather-display").offset().top
+         }, 1500);
+     });
+ }
+ 
+ /*** Begins execution when a place name is submited on Landing page ***/
+ function searchHikeTrails() {
+     $('.search-form').submit(function(event) {
+         event.preventDefault();
+         getWeatherData();
+     });
+     scrollpage();
+ };
+ 
+ /*** Begin ***/
+ $(searchHikeTrails);
